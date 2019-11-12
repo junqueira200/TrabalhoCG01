2853f1837fb8fb90c78bd40fa18ec21357b92fd8
igor <junqueira200@gmail.com>
2019-11-12 20:01:17 -0300

    a

---
+++ Normal.cpp
@@ -0,0 +1,47 @@
+//
+// Created by igor on 12/11/19.
+//
+
+#include "Normal.h"
+
+// Calculate normal
+vertex calcNormal(triangle t) {
+    vertex vn;
+    vertex v_0 = t.v[0],
+            v_1 = t.v[1],
+            v_2 = t.v[2];
+    vertex v1, v2;
+    double len;
+
+    /* Encontra vetor v1 */
+    v1.x = v_1.x - v_0.x;
+    v1.y = v_1.y - v_0.y;
+    v1.z = v_1.z - v_0.z;
+
+    /* Encontra vetor v2 */
+    v2.x = v_2.x - v_0.x;
+    v2.y = v_2.y - v_0.y;
+    v2.z = v_2.z - v_0.z;
+
+    /* Calculo do produto vetorial de v1 e v2 */
+    vn.x = (v1.y * v2.z) - (v1.z * v2.y);
+    vn.y = (v1.z * v2.x) - (v1.x * v2.z);
+    vn.z = (v1.x * v2.y) - (v1.y * v2.x);
+
+    /* normalizacao de n */
+    len = sqrt(pow(vn.x, 2) + pow(vn.y, 2) + pow(vn.z, 2));
+
+    vn.x /= len;
+    vn.y /= len;
+    vn.z /= len;
+
+    return vn;
+}
+
+// calculate and set GLUT normal
+void setCalcNormal(triangle t)
+{
+    vertex normal = calcNormal(t);
+    glNormal3f(normal.x, normal.y, normal.z);
+
+}
---
+++ Normal.h
@@ -0,0 +1,14 @@
+//
+// Created by igor on 12/11/19.
+//
+
+#include "Retangulo.h"
+
+#ifndef TRABALHOCG01_NORMAL_H
+#define TRABALHOCG01_NORMAL_H
+
+
+vertex calcNormal(triangle t);
+void setCalcNormal(triangle t);
+
+#endif //TRABALHOCG01_NORMAL_H
