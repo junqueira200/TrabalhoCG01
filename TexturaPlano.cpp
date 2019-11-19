6009c6cb7a1ff6f2d599670dd6af55d92c0c960f
igor <junqueira200@gmail.com>
2019-11-18 22:30:33 -0300

    Arquivos para texturas

---
+++ TexturaPlano.cpp
@@ -0,0 +1,7 @@
+//
+// Created by igor on 18/11/19.
+//
+
+#include "TexturaPlano.h"
+
+glcTexture *textureManager = new glcTexture();
---
+++ TexturaPlano.h
@@ -0,0 +1,11 @@
+//
+// Created by igor on 18/11/19.
+//
+#include "glcTexture.h"
+
+#ifndef TRABALHOCG01_TEXTURAPLANO_H
+#define TRABALHOCG01_TEXTURAPLANO_H
+
+extern glcTexture *textureManager;
+
+#endif //TRABALHOCG01_TEXTURAPLANO_H
